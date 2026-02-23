2aef11114e11413cc678dbdd9e56c7418a965eec
Niki <nikinatt@icloud.com>
2026-02-22 21:08:49 -0700

    Added Debouncer and two limit switches

--- src/main/java/frc/robot/commands/IntakeCommand.java
+++ src/main/java/frc/robot/commands/IntakeCommand.java
@@ -3,15 +3,16 @@ package frc.robot.commands;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.intake.Intake;
 
-public class RunIntakeCommand extends Command{
+public class IntakeCommand extends Command{
   private final Intake intake;
 
-  public RunIntakeCommand(Intake intake){
+  public IntakeCommand(Intake intake){
     this.intake = intake; 
 
   }
 
   public void execute(){
+    intake.extendIntake();
     intake.runIntake(); 
   }
 
--- src/main/java/frc/robot/subsystems/intake/Intake.java
+++ src/main/java/frc/robot/subsystems/intake/Intake.java
@@ -1,22 +1,67 @@
 package frc.robot.subsystems.intake;
 
+import edu.wpi.first.math.filter.Debouncer;
+import edu.wpi.first.wpilibj.DigitalInput;
 import frc.robot.util.RBSISubsystem;
 
 public class Intake{
   private final IntakeIO io;
+  private final DigitalInput limitSwitchExtended = new DigitalInput(0);
+  private final DigitalInput limitSwitchRetracted = new DigitalInput(1);
+  private final Debouncer debouncerOne = new Debouncer(0.05);
+  private final Debouncer debouncerTwo = new Debouncer(0.05);
 
   public Intake(IntakeIO io){
     this.io = io; 
-
-  }
-
-  public void extendIntake(){
-    io.setPercentOutputExtender(0.8);
-  }
-
-  public void runIntake(){
     io.setMode();
+  }
+
+
+  //returns true if limit switch is pressed 
+  private boolean limitSwitchExtended(){
+    return !debouncerOne.calculate(limitSwitchExtended.get());
+  }
+
+  private boolean limitSwitchRetracted(){
+    return !debouncerTwo.calculate(limitSwitchRetracted.get());
+  }
+
+  //returns true if both limit switches are pressed 
+  public boolean isExtended(){
+    return limitSwitchExtended(); 
+  }
+
+  public boolean isRetracted(){
+    return limitSwitchRetracted();
+  }
+
+  //false if limit switch is pressed, true if not pressed
+  public void extendIntake(){
+    if (!isExtended()) {
+    io.setPercentOutputExtender(0.8); //tune this number
+    }
+    else{
+      io.stopExtender();
+    }
+    io.setMode();
+  }
+
+  //only run intake if roller is down 
+  public void runIntake(){
+    if(isExtended()){
     io.setPercentOutputRoller(0.8);
+    }
+  }
+
+  public void stopIntake(){
+    io.stopRoller();
+  }
+
+  public void retractIntake(){
+    io.stopRoller();
+    if(isRetracted()){
+      io.setPercentOutputExtender(-0.8); //tune this number
+    }
   }
 
  
