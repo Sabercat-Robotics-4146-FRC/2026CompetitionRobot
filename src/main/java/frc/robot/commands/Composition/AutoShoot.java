7fe1d180dd742fd81e00f56b4d0dad7866eaa773
Niki <nikinatt@icloud.com>
2026-03-13 11:14:52 -0700

    increase shooter vel and home turret in auto

--- src/main/java/frc/robot/commands/Composition/AutoShoot.java
+++ src/main/java/frc/robot/commands/Composition/AutoShoot.java
@@ -1,5 +1,7 @@
 package frc.robot.commands.Composition;
 
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
@@ -7,11 +9,13 @@ import frc.robot.commands.Intake.PivotCommand;
 import frc.robot.subsystems.intake.Intake;
 import frc.robot.subsystems.kicker.Kicker;
 import frc.robot.subsystems.shooter.Shooter;
+import frc.robot.subsystems.turret.Turret;
 
 public class AutoShoot extends SequentialCommandGroup {
 
-  public AutoShoot(Kicker kicker, Shooter shooter, Intake intake) {
+  public AutoShoot(Kicker kicker, Shooter shooter, Intake intake, Turret turret) {
     addCommands(
+        Commands.runOnce(() -> turret.Home(), turret),
         new ParallelCommandGroup(new PivotCommand(intake), new ShootCommand(kicker, shooter)),
         new WaitCommand(5),
         new StopShootingCommand(kicker, shooter));
